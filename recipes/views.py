220a88d7728075d468de50e84d00a3d4e44a8897
Kaique Silva <kaique.silva@unimed197.local>
2022-02-23 17:16:48 -0300

    Templates inheritance and styling home cards

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from .fake_recipes import make_recipe
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, "recipes/index.html")
+    recipe_list = [make_recipe() for r in range(0, 10)]
+    context = {"recipes": recipe_list}
+    return render(request, "recipes/recipe_list.html", context)
+
+
+def recipe(request, id):
+    context = {"recipe": make_recipe()}
+    return render(request, "recipes/recipe_detail.html", context)
