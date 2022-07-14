3a2f4737cb54eb71460dd62063bff798dce7dc99
Kaique425 <motamendes.kaique.100@gmail.com>
2022-07-14 17:27:08 -0300

    wrinting the first model serializer to list recipes.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 
-from .views import site
+from .views import api, site
 
 app_name = "recipe"
 urlpatterns = [
     path("recipes/tags/<slug:slug>/", site.RecipeListViewTag.as_view(), name="tags"),
     path("theory/", site.theory, name="theory"),
-    path(
-        "api/v1/detail/<int:pk>", site.RecipeDetailViewApi.as_view(), name="api-detail"
-    ),
-    path("api/v1/home", site.RecipeListViewHomeApi.as_view(), name="api-home"),
     path("", site.RecipeListViewHome.as_view(), name="home"),
     path("recipe/<int:pk>/", site.RecipeDetailView.as_view(), name="recipe"),
     path(
@@ -18,4 +14,9 @@ urlpatterns = [
         name="category",
     ),
     path("recipe/search/", site.SearchRecipeListView.as_view(), name="search"),
+    path(
+        "api/v1/detail/<int:pk>", site.RecipeDetailViewApi.as_view(), name="api-detail"
+    ),
+    path("api/v1/home", site.RecipeListViewHomeApi.as_view(), name="api-home"),
+    path("recipe/api/v2/list", api.recipe_list, name="api-recipe-list"),
 ]
